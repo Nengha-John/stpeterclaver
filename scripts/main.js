ad0c27055a0cc78283d2a804fbb5f4f2b76fe62f
Nexus-John <68301183+Nexus-John@users.noreply.github.com>
2020-12-31 08:56:57 -0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//scroll effect on other pages
 window.onscroll = function(){scrollFunction()};
 
 function scrollFunction(height){
@@ -7,3 +8,45 @@ function scrollFunction(height){
         document.getElementById('header').style.height = "400px";
     }
 }
+
+//responsiveness for header
+window.addEventListener("load" ,function resized(){
+    let announce = document.querySelector("#news");
+    let small = window.innerWidth < 800 ;
+    announce.classList.toggle("news-resized", small);
+})
+
+window.addEventListener("resize",function resized(){
+    let announce = document.querySelector("#news");
+    let small = window.innerWidth < 800;
+    announce.classList.toggle("news-resized", small)
+   
+})
+
+window.addEventListener("resize", function(){
+    let welkom = document.querySelector(".welcome");
+    let small = window.innerWidth < 800;
+    welkom.classList.toggle("welcome-resized",small)
+})
+
+window.addEventListener("load",function(){
+    let welkom = document.querySelector(".welcome");
+    let small = window.innerWidth < 800;
+    welkom.classList.toggle("welcome-resized", small)
+})
+
+function fontchange(){
+    let welkom = document.querySelector(".welcome");
+    let widthh = window.innerWidth;
+    if (widthh < 500) {
+        welkom.style.fontSize = "5px";
+        welkom.style.lineHeight ="";
+        welkom.style.paddingTop ="";
+        welkom.style.visibility = "hidden";
+    }
+    
+
+window.addEventListener("load",fontchange());
+window.addEventListener("resize",fontchange());
+
+
